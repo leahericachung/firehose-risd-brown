49024e91c95a3247649c4fdcdc841138e288f242
Leah Chung <leahericachung@leah-chungs-mbp.risd.edu>
2014-02-08 17:11:11 -0500

    adding the form

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ end
 
 gem "twitter-bootstrap-rails"
 
+gem 'simple_form'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,22 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all 
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		# Pic.create(:hour => 1,
+		# :emotion => "Stoked",
+		# :learned => "HTML and CSS ROcks!")
+		Pic.create(pics_params) #this will shove the user input into our database
+		redirect_to pics_path #this tells you where to 'bounce' the user to once they click submit
+	end
+
+	private
+	def pics_params
+		params.require(:pic).permit(
+				:hour, :emotion, :learned)
+	end
 end
